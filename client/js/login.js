1fa5aba3d7ad84075c20788d621307a20b37eedf
denisvulcanio <denis@vulcan.io>
2021-10-18 06:04:14 +0000

    123

--- client/js/login.js
+++ client/js/login.js
@@ -22,3 +22,24 @@ $(document).ready(function(){
     })
   })
 });
+
+AtomicInteger aInt1 = new AtomicInteger(0);
+AtomicInteger aInt2 = new AtomicInteger(0);
+
+if (aInt1.equals(aInt2)) { ... }  // Noncompliant
+
+
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+      .antMatchers("/resources/**", "/signup", "/about").permitAll() // Compliant
+      .antMatchers("/admin/**").hasRole("ADMIN")
+      .antMatchers("/admin/login").permitAll() // Noncompliant; the pattern "/admin/login" should occurs before "/admin/**"
+      .antMatchers("/**", "/home").permitAll()
+      .antMatchers("/db/**").access("hasRole('ADMIN') and hasRole('DBA')") // Noncompliant; the pattern "/db/**" should occurs before "/**"
+      .and().formLogin().loginPage("/login").permitAll().and().logout().permitAll();
+  }
+
+GET /pageSomeWhere HTTP/1.1
+Host: webSite.com
+User-Agent: Mozilla/5.0
+Cookie: JSESSIONID=Hacked_Session_Value'''">
